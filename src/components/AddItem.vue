8244887a6035b4886970fe91ceebfe0a09c80b9c
isalindrawan <isalindrawan@outlook.com>
2020-05-11 23:30:38 +0700

    complete create, read, delete todo app

---
+++ src/components/AddItem.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <form @submit="add">
+      <input type="text" v-model="title" name="title" placeholder="Add Todo" />
+      <input type="submit" value="Submit" class="btn" />
+    </form>
+  </div>
+</template>
+
+<script>
+// import { v4 as uuidv4 } from 'uuid';
+
+export default {
+  name: 'AddItem',
+  data() {
+    return {
+      title: '',
+    };
+  },
+  methods: {
+    add(event) {
+      event.preventDefault();
+
+      const newItem = {
+        // id:uuidv4(),
+        title: this.title,
+        completed: false,
+      };
+
+      // send up to parent
+
+      this.$emit('add-todo', newItem);
+
+      this.title = '';
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+
+input[type='text'] {
+  flex: 10;
+  padding: 5px;
+}
+
+input[type='submit'] {
+  flex: 2;
+}
+</style>
